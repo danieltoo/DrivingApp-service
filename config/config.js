6766c387dccf2adc3efa56cca09d6d3f1b49e7ab
danieltoo <torresestradadaniel@gmail.com>
2018-10-04 15:56:37 -0500

    updates to docker

--- config/config.js
+++ config/config.js
@@ -9,12 +9,12 @@ exports.mysql = {
 exports.context = 'http://200.23.5.142:1026/v2';
 exports.crate = 'http://200.23.5.142:4200';*/
 
-exports.keyrock = process.env.KEYROCK
+exports.keyrock = "http://" + process.env.KEYROCK + ":" + process.env.KEYSTONE_PORT
 exports.mysql  = {
   host : process.env.MYSQL_HOST,
   db : process.env.MYSQL_DB,
   user : process.env.MYSQL_USER,
   password : process.env.MYSQL_PASSWORD
 };
-exports.context = process.env.ORION
-exports.crate = process.env.CRATEDB
+exports.context = "http://"+ process.env.ORION + ":1026/v2"
+exports.crate = process.env.CRATEDB + ":4200"
