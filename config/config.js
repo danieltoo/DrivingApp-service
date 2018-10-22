c64939a58f85f99602a03c8564d717211d4a38b6
danieltoo <torresestradadaniel@gmail.com>
2018-10-22 16:49:36 -0500

    Update config/config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 // Local Deployment 
-exports.keyrock = "http://viva-smartsdk.duckdns.org:8001"
+/*exports.keyrock = "http://viva-smartsdk.duckdns.org:8001"
 exports.mysql = {
   host : '200.23.5.142',
   db : 'smartsdksecurity',
@@ -8,10 +8,10 @@ exports.mysql = {
 }
 exports.context = 'http://35.185.120.11:1026/v2';
 exports.crate = 'http://35.185.120.11:4200';
-
+*/
 
 // Docker Deployment 
-/*exports.keyrock = "http://" + process.env.KEYROCK + ":" + process.env.KEYSTONE_PORT
+exports.keyrock = "http://" + process.env.KEYROCK + ":" + process.env.KEYSTONE_PORT
 exports.mysql  = {
   host : process.env.MYSQL_HOST,
   db : process.env.MYSQL_DB,
@@ -20,4 +20,3 @@ exports.mysql  = {
 };
 exports.context = "http://"+ process.env.ORION + ":1026/v2"
 exports.crate = process.env.CRATEDB + ":4200"
-*/
