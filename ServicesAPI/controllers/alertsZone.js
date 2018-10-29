9a3650c1c3fbe90661710e31706ad9962822792d
danieltoo <torresestradadaniel@gmail.com>
2018-10-29 16:30:16 -0600

    i added get all alerts of devices

--- ServicesAPI/controllers/alertsZone.js
+++ ServicesAPI/controllers/alertsZone.js
@@ -60,6 +60,9 @@ exports.getHistory = async function (req,res) {
 	});
 } 
 
+
+
+
 exports.getCurrent = async function (req,res) {
 	console.log(req.query)
     await Zone.findOne({where : { 'idZone': req.params.idZone }})
@@ -207,7 +210,57 @@ exports.getCurrentHour = async function (req,res) {
 	});
 } 
 
+exports.getAll = async function (req,res) {
+	console.log(req.query.id)
+    await Zone.findOne({where : { 'idZone': req.params.idZone }})
+    .then( async (zone) => {
+	  	if (zone != null){
+			let jsonQuery = {
+				type : "Alert",
+				options : "count",
+				georel :"coveredBy",
+				geometry:"polygon",
+				coords : zone.location
+			}
+			if(req.query.id != undefined)
+				jsonQuery["id"] = req.query.id
+			let queryToCount = ngsi.createQuery(jsonQuery);
 
+            await cb.getWithQuery(queryToCount)
+			.then(async (response) => {
+				let off = Number(response["headers"]["_headers"]["fiware-total-count"][0])
+				let params  = {
+					type : "Alert",
+					options : "keyValues",
+					georel :"coveredBy",
+					geometry:"polygon",
+					coords : zone.location,
+					limit : off,
+				}
+				if(req.query.id != undefined)
+					params["id"] = req.query.id
+
+				/*if (off > 10){
+					params.offset = off - 10
+				}*/
+				let query = ngsi.createQuery(params);
+				console.log(query)
+				await cb.getWithQuery(query)
+				.then((result) => {
+					res.set("fiware-total-count", off)
+					res.status(200).json(result.body.reverse())
+				})
+				.catch((error) =>{
+					res.status(500).send(error);
+                })
+			})
+			.catch((error) =>{
+				res.status(500).send(error);
+			})
+				
+	  	}  	
+	});
+} 
 
 
 
